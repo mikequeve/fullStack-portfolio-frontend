6cd4bbc739855bdd6ddf933271183d40fa5d1ade
mikequeve <michaelvega46@gmail.com>
2025-04-11 13:10:30 -0600

    Updating image og:metatag link...

--- src/Components/SeoMetaTags/SeoMetaTags.jsx
+++ src/Components/SeoMetaTags/SeoMetaTags.jsx
@@ -17,10 +17,6 @@ const SeoMetaTags = ({ title, description, name }) => {
         property='og:image'
         content='https://mike-vega.dev/portfolio-screenshot.png'
       />
-      <meta
-        property='og:image:secure_url'
-        content='https://mike-vega.dev/portfolio-screenshot.png'
-      />
       <meta property='og:image:type' content='image/png' />
       <meta property='og:image:width' content='200' />
       <meta property='og:image:alt' content={`Image of ${title} site`} />
