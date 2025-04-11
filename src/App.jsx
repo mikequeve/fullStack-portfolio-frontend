9e9c254d3e08621caeb4abe9a5cbcf72d3415031
mikequeve <michaelvega46@gmail.com>
2025-04-11 16:03:20 -0600

    Configuring analytics component...

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,11 @@
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import ProjectDetails from './Pages/ProjectDetails/ProjectDetails';
 import LandingPage from './Pages/LandingPage';
 import GlobalContextProvider from './context/GlobalContext';
 import { HelmetProvider } from 'react-helmet-async';
-import { useEffect } from 'react';
 
 function App() {
-  const location = useLocation();
-
-  useEffect(() => {
-    initGA();
-  }, []);
-
-  useEffect(() => {
-    trackPageView(location.pathname + location.search);
-  }, [location]);
-
   return (
     <>
       <HelmetProvider>
--- src/Pages/LandingPage.jsx
+++ src/Pages/LandingPage.jsx
@@ -11,10 +11,21 @@ import CertificateModal from '../Components/CertificateModal/CertificateModal';
 import SeoMetaTags from '../Components/SeoMetaTags/SeoMetaTags';
 import Footer from '../Components/Footer/Footer';
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
 const LandingPage = () => {
   const { projects, certificates, loading, isModalOpen, setIsModalOpen, selectedImg } =
     useContext(GlobalContext);
+
+  const location = useLocation();
+
+  useEffect(() => {
+    initGA();
+  }, []);
+
+  useEffect(() => {
+    trackPageView(location.pathname + location.search);
+  }, [location]);
 
   return (
     <>
